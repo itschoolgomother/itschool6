70a6dd56fb834507cdef4300b878bb08f5d06578
itschoolgomother <itschoolgomother@gmail.com>
2020-01-15 13:50:45 +0200

    15.1.2020

---
+++ AndreyPrima/main.js
@@ -0,0 +1,17 @@
+
+var test = prompt("grn","1000");
+
+var usd	 = 24;
+var euro = 26;
+var bitcoin = 193765;
+var plzol = 6;
+var bitcoincash = 6308;
+var afganistan = 0,31
+
+
+alert(Math.round(test/usd)+"  usd");
+alert(Math.round(test/euro)+"  euro");
+alert(Math.round(test/bitcoin)+"  bitcoin");
+alert(Math.round(test/plzol)+"  plzol");
+alert(Math.round(test/bitcoincash)+"  bitcoincash");
+alert(Math.round(test/afganistan)+"  afganistan");
