909a7e262ce181f7f72951646d1b77aa22867109
itschoolgomother <itschoolgomother@gmail.com>
2020-01-20 17:27:49 +0200

    20.1.2020

--- AndreyPrima/main.js
+++ AndreyPrima/main.js
@@ -1,17 +1,110 @@
 
-var test = prompt("grn","1000");
+var size = prompt("Size","10")
+var color = prompt("What do you want color","orange")
+getElementById("box");
+box.style.width = size+"px";
+box.style.height = size+"px";
+box.style.backgroundColor = color;
 
-var usd	 = 24;
-var euro = 26;
-var bitcoin = 193765;
-var plzol = 6;
-var bitcoincash = 6308;
-var afganistan = 0,31
+// var test = prompt("grn","1000");
+
+// var usd	 = 24;
+// var euro = 26;
+// var bitcoin = 193765;
+// var plzol = 6;
+// var bitcoincash = 6308;
+// var afganistan = 0,31
 
 
-alert(Math.round(test/usd)+"  usd");
-alert(Math.round(test/euro)+"  euro");
-alert(Math.round(test/bitcoin)+"  bitcoin");
-alert(Math.round(test/plzol)+"  plzol");
-alert(Math.round(test/bitcoincash)+"  bitcoincash");
-alert(Math.round(test/afganistan)+"  afganistan");
+// alert(Math.round(test/usd)+"  usd");
+// alert(Math.round(test/euro)+"  euro");
+// alert(Math.round(test/bitcoin)+"  bitcoin");
+// alert(Math.round(test/plzol)+"  plzol");
+// alert(Math.round(test/bitcoincash)+"  bitcoincash");
+// alert(Math.round(test/afganistan)+"  afganistan");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
