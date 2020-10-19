f3eb5f73733324d0a62804e9baf5d07f059509ed
itschoolgomother <itschoolgomother@gmail.com>
2020-10-19 14:04:27 +0300

    19.10.2020

---
+++ Sergej Liava/js.js
@@ -0,0 +1,12 @@
+function meat (N, C) {
+	let meat = (12-C)/N
+	return Math.floor(meat)
+}
+
+let num = 5;
+console.log("num")
+
+function getExchange (vidvaluti, kurs, suma){
+let kolvo = kurs*suma;
+ return `Ми Вам дадим $(kolvo) $(vidvaluti)` 
+}
